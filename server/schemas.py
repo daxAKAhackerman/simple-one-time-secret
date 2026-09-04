ff86f9da48157171e0d3ff889d9f94f32a6b241e
Samuel De Grace <48831884+daxAKAhackerman@users.noreply.github.com>
2026-09-04 11:33:58 -0400

    Feat - Address security issues (#56)

--- server/schemas.py
+++ server/schemas.py
@@ -1,8 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timezone
+from typing import Annotated
 
-from pydantic import BaseModel, Field
+from dateutil.relativedelta import relativedelta
+from pydantic import AfterValidator, BaseModel, Field
+
+
+def is_less_than_one_year_away(value: datetime) -> datetime:
+    today = datetime.now(tz=timezone.utc)
+    if value > today + relativedelta(months=1):
+        raise ValueError("Expiration must be less than a month")
+    return value
 
 
 class CreateSecret(BaseModel):
-    expiration: datetime
-    secret: str = Field(min_length=1)
+    expiration: Annotated[datetime, AfterValidator(is_less_than_one_year_away)]
+    secret: str = Field(min_length=1, max_length=900000)
